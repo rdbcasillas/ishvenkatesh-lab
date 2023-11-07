b616136032f19935730a5297ade71f5961831e17
manojkumar <mano2991@users.noreply.github.com>
2023-11-07 17:19:27 +0530

    update Arupam_1

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -126,7 +126,7 @@ export default {
           name: "Arupam Biswas",
           position: "Dissertation Student",
           image: "arupam",
-          desc: "Arupam Biswas joined the lab in August 2023 as a 1 year dissertate. He is the  final-year student of a five-year integrated M.Sc course (Life Sciences Major) at ISERC, Visva-Bharati, Santiniketan, India. His dissertation work is dedicated to enhancing High-Content Screening techniques to assess the impact of Transcription Factors in the context of spinal injuries. His academic interests revolve around Neuropsychiatric and Neurodegenerative Disorders. Beyond the lab, Either find him experimenting with delicious recipes in the kitchen or capturing moments of nature with his camera.",
+          desc: "Arupam Biswas joined the lab in August 2023 as a one year dissertation student. He is the  final-year student of a five-year integrated M.Sc course (Life Sciences Major) at ISERC, Visva-Bharati, Santiniketan, India. His dissertation work is dedicated to enhancing High-Content Screening techniques to assess the impact of Transcription Factors in the context of spinal injuries. His academic interests revolve around Neuropsychiatric and Neurodegenerative Disorders. Beyond the lab, Either find him experimenting with delicious recipes in the kitchen or capturing moments of nature with his camera.",
           email: "arupambiswas007@gmail.com",
         },
       ],
