bbd168a006760a12c4cdd553b7eef374ffeaae94
manojkumar <mano2991@users.noreply.github.com>
2024-02-01 10:03:03 +0530

    update_Sneha's_Joining

--- src/views/Funding.vue
+++ src/views/Funding.vue
@@ -10,7 +10,7 @@
             <li
               class="list-group-item d-flex justify-content-between align-items-center"
             >
-              SERB-SERG
+              SERB-SRG
               <span class="badge badge-primary badge-pill">2023-2026</span>
             </li>
           </ul>
